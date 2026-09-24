fc7a5ae5c7b07880cc44d4585ece2d7977b685cb
couto7x09092020-blip <couto7x09092020@gmail.com>
2026-09-24 21:15:56 +0000

    projetinho

--- Backend/server.js
+++ Backend/server.js
@@ -1,28 +1,21 @@
 const express = require("express");
 const cors = require("cors");
-//eles carregam o express e o cors
-// O express facilita a criação de servidores API, o Cors permite que o servidor receba requisições de outras origens
+
 const app = express();
-// express(); você está chamando o Express para criar uma nova aplicação, que é guardada dentro da variavel app
+
 app.use(cors());
-// aqui esta falando para usar o cors nessa aplicação
 app.use(express.json());
-// aqui esta falando para usar 
+
 app.get("/", (req, res) => {
-    res.send("API funcionando!");
-});
-app.get("/item",(req,res) =>{ //pef
-    const items = [{
+    const item = {
         nome: "processador",
-        preço: 2300,
-        categoria: "Hadware"
-    }];
-    res.json(items); //resposta dos items
+        preco: 2300,
+        categoria: "Hardware"
+    };
+
+    res.json(item);
 });
+
 app.listen(3000, () => {
     console.log("Servidor Rodando na porta 3000");
 });
-const url = "http://localhost:3000/item";
-fetch(url); // vai requisitar a url
-
-
